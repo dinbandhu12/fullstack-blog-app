b1ed30654c32973937272597574c0004e48ebea9
Dinbandhu12 <dinbandhu8445@gmail.com>
2024-10-15 20:53:42 +0530

    added layout code

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,19 @@
-import styles from "./homepage.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+
+<h1>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos est id reprehenderit minima soluta enim adipisci eius in voluptatibus dolor magni saepe, explicabo fugit voluptatum, accusantium error tenetur perspiciatis. Iure.
+</h1>
+
+    <Link href="/blog">Blog
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam repellendus omnis qui animi ullam totam quam dicta placeat ipsam. Inventore eius voluptates eum excepturi repellendus sed minima. Eius, voluptatem ullam?
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam repellendus omnis qui animi ullam totam quam dicta placeat ipsam. Inventore eius voluptates eum excepturi repellendus sed minima. Eius, voluptatem ullam?
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam repellendus omnis qui animi ullam totam quam dicta placeat ipsam. Inventore eius voluptates eum excepturi repellendus sed minima. Eius, voluptatem ullam?
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam repellendus omnis qui animi ullam totam quam dicta placeat ipsam. Inventore eius voluptates eum excepturi repellendus sed minima. Eius, voluptatem ullam?
+    
+    </Link>
+    Home page
+  </div>;
 }
