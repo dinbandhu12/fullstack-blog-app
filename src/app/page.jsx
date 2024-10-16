d13a9183c63fa7c6338dfa529d1cf26acd3a02c5
Dinbandhu12 <dinbandhu8445@gmail.com>
2024-10-16 22:23:27 +0530

    added nav links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,21 @@
-import Footer from "@/components/footer/Footer";
-import Navbar from "@/components/navbar/Navbar";
 import Link from "next/link";
+import styles from "./homepage.module.css"
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/Menu/Menu";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <Footer />
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+
+      </div>
+
 
     </div>
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import styles from './navbar.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      Navbar
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt='facebook' width={24} height={24} />
+        <Image src="/instagram.png" alt='facebook' width={24} height={24} />
+        <Image src="/tiktok.png" alt='facebook' width={24} height={24} />
+        <Image src="/youtube.png" alt='facebook' width={24} height={24} />
+      </div>
+      <div className={styles.logo}>DBS Blog</div>
+      <div className={styles.links}>
+        <Link href='/'>Home</Link>
+        <Link href='/'>Contact</Link>
+        <Link href='/'>About</Link>
+      </div>
     </div>
   )
 }
