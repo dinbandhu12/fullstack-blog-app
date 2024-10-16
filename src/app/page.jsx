9deb236fcfa71cc4cf1d72f7a7662732885a7489
Dinbandhu12 <dinbandhu8445@gmail.com>
2024-10-16 21:07:41 +0530

    small change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,15 @@
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
 import Link from "next/link";
 
 export default function Home() {
-  return <div>
+  return (
+    <div>
+      <Navbar />
+      <Footer />
 
-<h1>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos est id reprehenderit minima soluta enim adipisci eius in voluptatibus dolor magni saepe, explicabo fugit voluptatum, accusantium error tenetur perspiciatis. Iure.
-</h1>
+    </div>
 
-    <Link href="/blog">Blog
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam repellendus omnis qui animi ullam totam quam dicta placeat ipsam. Inventore eius voluptates eum excepturi repellendus sed minima. Eius, voluptatem ullam?
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam repellendus omnis qui animi ullam totam quam dicta placeat ipsam. Inventore eius voluptates eum excepturi repellendus sed minima. Eius, voluptatem ullam?
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam repellendus omnis qui animi ullam totam quam dicta placeat ipsam. Inventore eius voluptates eum excepturi repellendus sed minima. Eius, voluptatem ullam?
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam repellendus omnis qui animi ullam totam quam dicta placeat ipsam. Inventore eius voluptates eum excepturi repellendus sed minima. Eius, voluptatem ullam?
-    
-    </Link>
-    Home page
-  </div>;
+  )
+
 }
