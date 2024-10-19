9622c3cd91e73fb3a5598c002f5c27849cb4ebfd
Dinbandhu12 <dinbandhu8445@gmail.com>
2024-10-19 22:09:53 +0530

    added category and post cards

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import styles from "./card.module.css";
+import Link from "next/link";
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt="postImg" fill className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>Feb 12, 2022 - </span>
+          <span className={styles.category}>Culture</span>
+        </div>
+        <Link href="/" className={styles.heading}>
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        </Link>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+          corporis mollitia, iste dignissimos laboriosam exercitationem magni
+          numquam assumenda, eos ab labore. Debitis, totam.
+        </p>
+        <Link href="/" className={styles.readMoreBtn}>
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,12 +1,87 @@
-import React from 'react'
-import styles from './categoryList.module.css'
+import React from "react";
+import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const CategoryList = () => {
   return (
     <div className={styles.container}>
-      CategoryList
-    </div>
-  )
-}
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
 
-export default CategoryList
+        {/* change the category names latter which are related to the software engineering*/}
+        {/* some catergory [TechTrends], [AI], [SE], [Story], [Career], [Challenges] */}
+
+        <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+          <Image
+            src="/style.png"
+            alt="style"
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          <span className={styles.categoryName}>Style</span>
+        </Link>
+
+        
+        <Link href={`/blog`} className={`${styles.category} ${styles.fashion}`}>
+          <Image
+            src="/fashion.png"
+            alt="fashion"
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          <span className={styles.categoryName}>Fashion</span>
+        </Link>
+
+        <Link href={`/blog`} className={`${styles.category} ${styles.food}`}>
+          <Image
+            src="/food.png"
+            alt="food"
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          <span className={styles.categoryName}>Food</span>
+        </Link>
+
+        <Link href={`/blog`} className={`${styles.category} ${styles.travel}`}>
+          <Image
+            src="/travel.png"
+            alt="travel"
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          <span className={styles.categoryName}>Travel</span>
+        </Link>
+
+        <Link href={`/blog`} className={`${styles.category} ${styles.culture}`}>
+          <Image
+            src="/culture.png"
+            alt="culture"
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          <span className={styles.categoryName}>Culture</span>
+        </Link>
+
+        <Link href={`/blog`} className={`${styles.category} ${styles.coding}`}>
+          <Image
+            src="/coding.png"
+            alt="coding"
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          <span className={styles.categoryName}>Coding</span>
+        </Link>
+
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
